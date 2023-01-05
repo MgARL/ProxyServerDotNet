f3c9f263261bd61e43ba4bcccbfd1dd2b5fbf688
Miguel Rodriguez <mgarl22@gmail.com>
2023-01-04 19:42:14 -0500

    - WIP - FInishes few basic routes - Created manager to handle dishes.

---
+++ ProxyServerDotNet/Managers/DishesManger.cs
@@ -0,0 +1,43 @@
+﻿using FireSharp.Response;
+using ProxyServerDotNet.Manager;
+using ProxyServerDotNet.Models;
+using System.Collections.Generic;
+
+namespace ProxyServerDotNet.Managers
+{
+    public class DishesManger
+    {
+        static async public Task<List<Dish>> GenerateFiveRandomDishes()
+        {
+            var dishes = await FireBaseManager.GetAllDishes();
+            var  randomDishes = new List<Dish>();
+            if (dishes.Any())
+            {
+                var random = new Random();
+                randomDishes = dishes.OrderBy(d => random.Next()).ToList();
+
+                randomDishes.RemoveRange(5, randomDishes.Count - 5);
+            }
+
+            return randomDishes;
+        }
+
+        static async public Task<SetResponse> AddDish(Dish newDish)
+        {
+            var client = FireBaseManager.initialConfig();
+            var allDishes = await FireBaseManager.GetAllDishes();
+            allDishes.Add(newDish);
+
+            return await client.SetAsync("dishesList", allDishes); ;
+        }
+
+        static async public Task<SetResponse> DeleteDish(int id)
+        {
+            var client = FireBaseManager.initialConfig();
+            var allDishes = await FireBaseManager.GetAllDishes();
+            allDishes.RemoveAt(id); 
+
+            return await client.SetAsync("dishesList", allDishes); ;
+        }
+    }
+}
